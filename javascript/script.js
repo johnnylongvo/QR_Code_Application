d42fca49a296f21488c1b278eadd96c0bd42b372
johnnylongvo <johnnylongvo@gmail.com>
2022-08-16 13:47:41 -0500

    show & hide spinner functions.

--- javascript/script.js
+++ javascript/script.js
@@ -8,6 +8,26 @@ const onGenerateSubmit = (e) => {
     const size = document.getElementById('size').value;
     
     console.log(url, size);
+    // validation 
+    if (url == '') {
+        alert('No URL found! Please enter a URL!');
+    } else {
+        showLoadingSpinner();
+
+        setTimeout(() => {  // show loading spinner for 1 second or so.
+            hideLoadingSpinner();
+        }, 1000); 
+    }
 };
+
+const showLoadingSpinner = () => {
+    document.getElementById('loadingSpinner').style.display = 'block';
+}
+
+const hideLoadingSpinner = () => {
+    document.getElementById('loadingSpinner').style.display = 'none';
+}
+
+hideLoadingSpinner();
 
 form.addEventListener('submit', onGenerateSubmit);
